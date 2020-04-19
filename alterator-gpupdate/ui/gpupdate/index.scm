ef8c0daddd71a3efae81bc333a77bca319109a43
Evgeny Sinelnikov <sin@altlinux.org>
2020-04-19 23:54:31 +0400

    Add support for multiple profiles

--- alterator-gpupdate/ui/gpupdate/index.scm
+++ alterator-gpupdate/ui/gpupdate/index.scm
@@ -14,9 +14,11 @@
                 (let ((data (woo-read-first "/gpupdate")))
                   (form-update-value-list '("gp_status" "gp_type") data)
                   (document:popup-information
-                    (string-append
-                      (_ "Group Policy ")
-                      (woo-get-option data 'gp_type))
+                    (if (woo-get-option data 'gp_status)
+                      (string-append
+                      (_ "Group Policy enabled for profile: ")
+                        (_ (woo-get-option data 'gp_type)))
+                      (_ "Group Policy disabled"))
                     'ok))
             )
         )
@@ -27,23 +29,23 @@
 (define (ui-init)
     (let ((data (woo-read-first "/gpupdate")))
         (form-update-value-list '("gp_status" "gp_type") data)
-
-        (if (woo-get-option data 'gp_status)
-            (begin (gp-type-activity   activity   #t) )
-        ;;;else branch
-            (begin (gp-type-activity   activity   #f) )
-        )
+        (reset-gp-type-activity)
     )
 )
 
 (define (reset-gp-type-activity)
     (if (form-value "gp_status")
         (begin (gp-type-activity   activity   #t) )
-    ;;;else branch
         (begin (gp-type-activity   activity   #f) )
     )
 )
 
+;;; Predefined profile names with l10n
+(define profile-workstation-name (_ "workstation"))
+(define profile-server-name (_ "server"))
+
+(define (add-profile-radio profile)
+    (radio name "gp_type" value profile text (_ profile) state #f))
 
 ;;; UI
 (gridbox
@@ -54,7 +56,9 @@
     (checkbox colspan 2 align "left" text(_ "Group Policy Management") name "gp_status" (when changed (reset-gp-type-activity)) )
     ;;; Warning if task-auth-ad and task-auth-ad-sssd is unavailable
     (document:id type-group-policy-warning (gridbox colspan 4 columns "0;100" visibility #t (label text "   ") (label
-        text (string-append (bold (_ "Warning: ")) (_ "This module enables Group Policy users and computer<br />management. Additional settings applies during boot process and<br />user login after authentication in Active Directory.")))))
+        text (string-append (bold (_ "Warning: ")) (_ "This module enables Group Policy users and computer
+<br />management. Additional settings applies during boot process and
+<br />user login after authentication in Active Directory.")))))
     (spacer)
     (label)
 
@@ -63,11 +67,7 @@
             (label text (_ "Current group policy profile:") align "left" visibility #t)
             (spacer)
 
-            ;;; Workstation
-            (radio name "gp_type" value "workstation" text (_ "Workstation") state #t)
-
-            ;;; Server
-            (radio name "gp_type" value "server" text (_ "Server"))
+            (map add-profile-radio (woo-list/name "/gpupdate/profiles"))
         )
     )
 
